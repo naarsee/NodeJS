5ee0dc0135323072d46987d81b5328eb69885e97
Ignacio Ordaz <narseg.f@gmail.com>
2024-05-12 18:18:39 -0600

    agrego bd

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@ const {Pool}=require('pg');
 
 //en esta sección de codigo hare la conexion a la db
 //estarán mis datos de mis postgres favor de poner los propios
-
+//no los agregue al .env para su manipulacion sencilla si ustedes cambian algo
 const database=new Pool({
     user:'postgres',
     password:'naarsee',
